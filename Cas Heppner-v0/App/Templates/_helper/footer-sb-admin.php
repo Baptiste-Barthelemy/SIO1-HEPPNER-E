0989ce87beea6c63e31473884e64010aebddf1de
jeulis <jeulis8@gmail.com>
2018-05-23 09:28:28 +0200

    Baptiste

--- Cas Heppner-v0/App/Templates/_helper/footer-sb-admin.php
+++ Cas Heppner-v0/App/Templates/_helper/footer-sb-admin.php
@@ -34,19 +34,19 @@
       </div>
     </div>
     <!-- Bootstrap core JavaScript-->
-    <script src="sb-admin/vendor/jquery/jquery.min.js"></script>
-    <script src="sb-admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="www/sb-admin/vendor/jquery/jquery.min.js"></script>
+    <script src="www/sb-admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- Core plugin JavaScript-->
-    <script src="sb-admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="www/sb-admin/vendor/jquery-easing/jquery.easing.min.js"></script>
     <!-- Page level plugin JavaScript-->
-    <script src="sb-admin/vendor/chart.js/Chart.min.js"></script>
-    <script src="sb-admin/vendor/datatables/jquery.dataTables.js"></script>
-    <script src="sb-admin/vendor/datatables/dataTables.bootstrap4.js"></script>
+    <script src="www/sb-admin/vendor/chart.js/Chart.min.js"></script>
+    <script src="www/sb-admin/vendor/datatables/jquery.dataTables.js"></script>
+    <script src="www/sb-admin/vendor/datatables/dataTables.bootstrap4.js"></script>
     <!-- Custom scripts for all pages-->
-    <script src="sb-admin/js/sb-admin.min.js"></script>
+    <script src="www/sb-admin/js/sb-admin.min.js"></script>
     <!-- Custom scripts for this page-->
-    <script src="sb-admin/js/sb-admin-datatables.min.js"></script>
-    <script src="sb-admin/js/sb-admin-charts.min.js"></script>
+    <script src="www/sb-admin/js/sb-admin-datatables.min.js"></script>
+    <script src="www/sb-admin/js/sb-admin-charts.min.js"></script>
   </div>
 </body>
 
