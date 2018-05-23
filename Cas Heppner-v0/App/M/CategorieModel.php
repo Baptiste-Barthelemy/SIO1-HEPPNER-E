fec8436f074f1eb6087c12d8bdfe7830a790ff3f
jeulis <jeulis8@gmail.com>
2018-05-23 14:39:25 +0200

    Théo

--- Cas Heppner-v0/App/M/CategorieModel.php
+++ Cas Heppner-v0/App/M/CategorieModel.php
@@ -16,4 +16,15 @@ class CategorieModel extends Object
     public $id;
     public $libelle;
     public $permis_did;
+
+
+    public function nbVehicules(){
+        //select * from modeles where categorie_id=?
+        $modeles = ModeleModel::find(['categorie_id'=>$this-id]);
+        $nb = 0;
+        foreach ($modeles as $modele) {
+            $nb+=$modele->getNbVehicules();
+        }
+        return $nb;
+    }
 }
