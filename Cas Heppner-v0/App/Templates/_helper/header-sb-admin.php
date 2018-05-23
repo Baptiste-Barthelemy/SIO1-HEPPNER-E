baf3d7646d0734ed3c9e6fba0fbc352f5428981b
jeulis <jeulis8@gmail.com>
2018-05-23 14:20:58 +0200

    Théo

--- Cas Heppner-v0/App/Templates/_helper/header-sb-admin.php
+++ Cas Heppner-v0/App/Templates/_helper/header-sb-admin.php
@@ -18,13 +18,17 @@
     <link href="www/sb-admin/css/sb-admin.css" rel="stylesheet">
 </head>
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
+
 <!-- Navigation-->
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
     <a class="navbar-brand" href=""><?php echo \Core\Config::APPLI_NAME; ?></a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarResponsive">
+
         <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
                 <a class="nav-link" href="">
