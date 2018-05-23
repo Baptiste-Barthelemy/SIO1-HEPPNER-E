d67c3de4528ea31ff4f59b9c34869cd32ef79827
Baptiste-Barthelemy <35603700+Baptiste-Barthelemy@users.noreply.github.com>
2018-05-23 17:24:42 +0200

    Add files via upload

---
+++ Cas Heppner-v0/App/Templates/DashBoard/parametresTemplate.php
@@ -0,0 +1,87 @@
+<ol class="breadcrumb">
+    <li class="breadcrumb-item">
+        <a href="#">Paramètres</a>
+    </li>
+</ol>
+
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+<div class="container">
+    <div class="card card-login mx-auto mt-5">
+        <div class="card-header text-center text-white bg-dark">Paramètres</div>
+        <div class="card-body text-center">
+                <div class="form-group">
+                    <label for="devis">Nombres de devis a réaliser :</label></br>
+                    <input type="checkbox" name="1">
+                </div>
+            <div class="form-group">
+                <label for="devis">Nombres de conducteurs en déplacement :</label></br>
+                <input type="checkbox" name="2">
+            </div>
+            <div class="form-group">
+                <label for="devis">Listes des conducteurs en déplacements :</label></br>
+                <input type="checkbox" name="3">
+            </div>
+            <div class="form-group">
+                <label for="devis">Chiffre d'affaire du jours :</label></br>
+                <input type="checkbox" name="4">
+            </div>
+            <div class="form-group">
+                <label for="type">Chiffre d'affaire du mois :</label><br />
+                <SELECT name="type" size="1">
+                    <OPTION>Janvier-01
+                    <OPTION>Fevrier-02
+                    <OPTION>Mars-03
+                    <OPTION>Avril-04
+                    <OPTION>Mai-05
+                    <OPTION>Juin-06
+                    <OPTION>Juillet-07
+                    <OPTION>Aout-08
+                    <OPTION>Septembre-09
+                    <OPTION>Octobre-10
+                    <OPTION>Novembre-11
+                    <OPTION>Decembre-12
+                </SELECT>
+            <div class="form-group">
+                <label for="devis">Nombres de kilomètres parcourus:</label></br>
+                <input type="checkbox" name="5">
+            </div>
+            <div class="form-group">
+                <label for="devis">Chiffre d'affaire par client :</label></br>
+                <input type="checkbox" name="6">
+            </div>
+            <div class="form-group">
+                <label for="devis">Poids transporté par type de marchandise :</label></br>
+                <input type="checkbox" name="7">
+            </div>
+            <div class="form-group">
+                <label for="devis">Nombre total de total de véhicules :</label></br>
+                <input type="checkbox" name="8">
+            </div>
+            <div class="form-group">
+                <label for="devis">Nombre total de conducteurs :</label></br>
+                <input type="checkbox" name="9">
+            </div>
+            <div class="form-group">
+                <label for="devis">Nombre de véhicules par catégorie :</label></br>
+                <input type="checkbox" name="10">
+            </div>
+            <div class="form-group">
+                <label for="devis">Kilométrage moyen de chaque véhicule :</label></br>
+                <input type="checkbox" name="11">
+            </div>
+            <div class="form-group">
+                <label for="devis">Le trajet le plus utilisé :</label></br>
+                <input type="checkbox" name="12">
+            </div>
+            <div class="form-group">
+                <label for="devis">Le plus long trajet réalisé :</label></br>
+                <input type="checkbox" name="13">
+            </div>
+                <div class="form-group">
+                    <label for="valider">Valider :</label></br>
+                    <input type="submit" name="valider">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>';
