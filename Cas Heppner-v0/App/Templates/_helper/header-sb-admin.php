ed0ec2a15790edcf014ec762734512be08f284b8
jeulis <jeulis8@gmail.com>
2018-05-23 14:24:20 +0200

    Baptiste

--- Cas Heppner-v0/App/Templates/_helper/header-sb-admin.php
+++ Cas Heppner-v0/App/Templates/_helper/header-sb-admin.php
@@ -51,22 +51,6 @@
         </ul>
     </div>
 
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-                <a class="nav-link" href="">
-                    <span class="nav-link-text">Tableau de bord</span>
-                </a>
-            </li>
-        </ul>
-        <ul class="navbar-nav sidenav-toggler">
-            <li class="nav-item">
-                <a class="nav-link text-center" id="sidenavToggler">
-                    <i class="fa fa-fw fa-angle-left"></i>
-                </a>
-            </li>
-        </ul>
-    </div>
 
 </nav>
 <div class="content-wrapper">
