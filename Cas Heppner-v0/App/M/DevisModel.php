515e43b1bd4b7744722561e5946b5a99e6d04105
emilienjea <39518449+emilienjea@users.noreply.github.com>
2018-05-24 15:10:53 +0200

    Update DevisModel.php

--- Cas Heppner-v0/App/M/DevisModel.php
+++ Cas Heppner-v0/App/M/DevisModel.php
@@ -11,7 +11,7 @@ use Core\Object;
 
 class DevisModel extends Object
 {
-    protected static $_table='devis';
+    protected static $_table = 'devis';
 
     public $id;
     public $demandeTransport_id;
@@ -19,4 +19,53 @@ class DevisModel extends Object
     public $dateEnvoi;
     public $dateArriveePrevue;
     public $valide;
+
+    public static function getChiffreAffaireJour(){
+        $listeDevis=DevisModel::getAll();
+        $CAJour=0;
+        $dateJour=new \DateTime("NOW");
+
+        foreach ($listeDevis as $unDevis){
+            $dateAujourdhui = new \DateTime($unDevis->getDateEnvoi());
+            if($dateJour==$dateAujourdhui){
+                $CAJour+=$unDevis->getMontant();
+            }
+        }
+        return $CAJour;
+
+    }
+//chiffre affaire mois
+    public static function getChiffreAffaireMois(){
+        $listeDevis=DevisModel::getAll();
+        $CAMois=0;
+        $dateJour=new \DateTime("NOW");
+
+        foreach ($listeDevis as $unDevis){
+            $dateAujourdhui = new \DateTime($unDevis->getDateEnvoi());
+            if($dateJour->format('m')==$dateAujourdhui->format('m')){
+                $CAMois+=$unDevis->getMontant();
+            }
+        }
+        return $CAMois;
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMontant()
+    {
+        return $this->montant;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateEnvoi()
+    {
+        return $this->dateEnvoi;
+    }
+
+
+
 }
